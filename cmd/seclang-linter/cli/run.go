678a651a41341f638f482de8f9470835fa8887f6
Minsu Park <me@minsu.digital>
2025-03-13 15:13:00 -0500

    Implement a simple file reader

--- cmd/seclang-linter/cli/run.go
+++ cmd/seclang-linter/cli/run.go
@@ -1,6 +1,11 @@
 package cli
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/bak-minsu/seclang-linter/internal/parse"
+	"github.com/spf13/cobra"
+)
 
 const runDescription = `
 Run linters on files within given paths.
@@ -15,7 +20,9 @@ var runCmd = &cobra.Command{
 	Use:   "run [OPTIONS] <path to seclang file> <additional paths to seclang files>...",
 	Short: "Runs linter on given paths",
 	Long:  runDescription,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		return nil
+	Run: func(cmd *cobra.Command, args []string) {
+		if _, err := parse.ParseFile(args[0]); err != nil {
+			fmt.Println(err)
+		}
 	},
 }
--- internal/parse/parse.go
+++ internal/parse/parse.go
@@ -2,6 +2,7 @@ package parse
 
 import (
 	"fmt"
+	"os"
 )
 
 func Parse(content []byte) (*File, error) {
@@ -16,4 +17,24 @@ func Parse(content []byte) (*File, error) {
 	return &File{
 		Directives: directives,
 	}, nil
+}
+
+func ParseFile(name string) (*File, error) {
+	contents, err := os.ReadFile(name)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"could not read file %q: %w",
+			name,
+			err,
+		)
+	}
+
+	parsed, err := Parse(contents)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"could not parse read file contents: %w", err,
+		)
+	}
+
+	return parsed, nil
 }
